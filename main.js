9b9d096c115aa4e6640226bd093841bf42813bbb
Klaus Förster <klaus.foerster@uibk.ac.at>
2024-05-02 11:55:01 +0200

    Conditional (ternary) operator bei alle Messwerten verwenden

--- main.js
+++ main.js
@@ -55,10 +55,10 @@ async function showStations(url) {
             layer.bindPopup(`
                 <h4>${feature.properties.name} (${feature.geometry.coordinates[2]}m)</h4>
                 <ul>
-                    <li>Lufttemperatur (°C): ${feature.properties.LT || "-"}</li>
-                    <li>Relative Luftfeuchte (%): ${feature.properties.RH || "-"}</li>
-                    <li>Windgeschwindigkeit (km/h): ${feature.properties.WG ||"-"}</li>
-                    <li>Schneehöhe (cm): ${feature.properties.HS || "-"}</li>
+                    <li>Lufttemperatur (°C): ${feature.properties.LT != undefined ? feature.properties.LT : "-"}</li>
+                    <li>Relative Luftfeuchte (%): ${feature.properties.RH != undefined ? feature.properties.RH : "-"}</li>
+                    <li>Windgeschwindigkeit (km/h): ${feature.properties.WG != undefined ? feature.properties.WG : "-"}</li>
+                    <li>Schneehöhe (cm): ${feature.properties.HS != undefined ? feature.properties.HS : "-"}</li>
                 </ul>
                 <span>${feature.properties.date}</span>
             `);
