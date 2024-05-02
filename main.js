34383677b5e9b9c7e40796a5d24e09d26f30bbf5
Klaus Förster <klaus.foerster@uibk.ac.at>
2024-05-02 16:35:48 +0200

    add filter for temperature

--- main.js
+++ main.js
@@ -38,11 +38,17 @@ L.control.scale({
 
 function showTemperature(geojson) {
     L.geoJSON(geojson, {
+        filter: function(feature) {
+            // feature.properties.LT
+            if (feature.properties.LT > -50 && feature.properties.LT < 50) {
+                return true;
+            }
+        },
         pointToLayer: function(feature, latlng) {
             return L.marker(latlng, {
                 icon: L.divIcon({
                     className: "aws-div-icon",
-                    html: `<span>${feature.properties.LT}</span>`
+                    html: `<span>${feature.properties.LT.toFixed(1)}</span>`
                 })
             })
         }
