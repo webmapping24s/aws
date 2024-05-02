09a54457f4472ed2cc4142a6ea6d0bb32a2ee107
Klaus Förster <klaus.foerster@uibk.ac.at>
2024-05-02 11:17:38 +0200

    Template ausgepackt

---
+++ main.js
@@ -0,0 +1,46 @@
+/* Wetterstationen Euregio Beispiel */
+
+// Innsbruck
+let ibk = {
+    lat: 47.267222,
+    lng: 11.392778
+};
+
+// Karte initialisieren
+let map = L.map("map", {
+    fullscreenControl: true
+}).setView([ibk.lat, ibk.lng], 11);
+
+// thematische Layer
+let themaLayer = {
+    stations: L.featureGroup().addTo(map)
+}
+
+// Hintergrundlayer
+L.control.layers({
+    "Relief avalanche.report": L.tileLayer(
+        "https://static.avalanche.report/tms/{z}/{x}/{y}.webp", {
+        attribution: `© <a href="https://sonny.4lima.de">Sonny</a>, <a href="https://www.eea.europa.eu/en/datahub/datahubitem-view/d08852bc-7b5f-4835-a776-08362e2fbf4b">EU-DEM</a>, <a href="https://lawinen.report/">avalanche.report</a>, all licensed under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>`
+    }).addTo(map),
+    "Openstreetmap": L.tileLayer.provider("OpenStreetMap.Mapnik"),
+    "Esri WorldTopoMap": L.tileLayer.provider("Esri.WorldTopoMap"),
+    "Esri WorldImagery": L.tileLayer.provider("Esri.WorldImagery")
+}, {
+    "Wetterstationen": themaLayer.stations
+}).addTo(map);
+
+// Maßstab
+L.control.scale({
+    imperial: false,
+}).addTo(map);
+
+// GeoJSON der Wetterstationen laden
+async function showStations(url) {
+    let response = await fetch(url);
+    let geojson = await response.json();
+
+    // Wetterstationen mit Icons und Popups
+    console.log(geojson)
+
+}
+showStations("https://static.avalanche.report/weather_stations/stations.geojson");
